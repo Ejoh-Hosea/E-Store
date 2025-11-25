925d013088c2bb9eac6ff6dd80bf9e77fda50fbd
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-25 11:45:24 -0500

    custom class (pages centered)

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -6,7 +6,9 @@ const HomeLayout = () => {
       <nav>
         <h1 className=" text-primary">Navbar</h1>
       </nav>
-      <Outlet />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   );
 };
