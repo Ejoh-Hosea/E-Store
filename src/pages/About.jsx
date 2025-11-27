a1263cc724d6e708fb193f663f4931560d053055
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-11-26 23:56:53 -0500

    About page Structure and styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,27 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We Love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              E-Shop
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+        eligendi eaque illo quis ipsa laboriosam deserunt tempore corporis
+        fugiat. Vel tempore voluptates odio, saepe suscipit, necessitatibus
+        facere perspiciatis, minima dolorum quibusdam ipsam eos velit earum cum
+        magnam numquam deleniti! Adipisci iste omnis iusto enim earum ipsam
+        fugit ab id repellendus.
+      </p>
+    </>
+  );
 };
 export default About;
