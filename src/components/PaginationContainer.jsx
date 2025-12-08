c1c56a19f5072305655942076f35709d5dff1867
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-12-08 13:15:08 -0500

    Pagination for product page complete

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -9,8 +9,12 @@ const PaginationContainer = () => {
   const pages = Array.from({ length: pageCount }, (_, index) => {
     return index + 1;
   });
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
   const handlePageChange = (pageNumber) => {
-    console.log(pageNumber);
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
   };
   if (pageCount < 2) return null;
   return (
@@ -19,7 +23,9 @@ const PaginationContainer = () => {
         <button
           className="btn btn-xs sm:btn-md join-item"
           onClick={() => {
-            handlePageChange(1);
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
           }}
         >
           Prev
@@ -40,7 +46,9 @@ const PaginationContainer = () => {
         <button
           className="btn btn-xs sm:btn-md join-item"
           onClick={() => {
-            handlePageChange(1);
+            let nextPage = page + 1;
+            if (nextPage < 1) nextPage = 1;
+            handlePageChange(nextPage);
           }}
         >
           Next
