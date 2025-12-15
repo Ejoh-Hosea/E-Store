211d8c91449893cec8ff00158b7217ade090256b
Hosea54 <ejohhoseamwongwe@gmail.com>
2025-12-15 14:49:19 -0500

    checkout page complete

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,11 +1,51 @@
 import { Form, redirect } from "react-router-dom";
 import FormInput from "./FormInput";
 import SubmitBtn from "./SubmitBtn";
+import { customFetch, formatPrice } from "../utils";
+import { clearCart } from "../features/cart/cartSlice";
+import { toast } from "react-toastify";
 
-export const action = async (store) => {
-  console.log(store);
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, address } = Object.fromEntries(formData);
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+    console.log(store);
+
+    const info = {
+      name,
+      address,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+    try {
+      const response = await customFetch.post(
+        "/orders",
+        { data: info },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      store.dispatch(clearCart());
+      toast.success("order placed successfully");
+
+      return redirect("/orders");
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "please double check your credentials";
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const CheckoutForm = () => {
   return (
